d9258126ca66412118abe0be46a96716f432a31f
athompson-kz1991 <alexthompson1991@hotmail.com>
2016-05-29 22:16:23 -0700

    Initial pdf/lyx work, produced GMV porfolio for part 1.

--- r_code/BTgmvLoBoxWeekly104.R
+++ r_code/BTgmvLoBoxWeekly104.R
@@ -7,6 +7,7 @@ library(lattice)
 returns = smallcapW    # Weekly returns 1997 throug 2010
 MARKET = returns[,"Weekvwretd"]
 returns = smallcapW[,1:20]
+
 # plot.zoo(returns, main = "SMALL CAPS")
 
 funds = colnames(returns)
@@ -44,22 +45,42 @@ ret.comb <- na.omit(merge(GMV.LO, GMV.BOX, MARKET, all=F))
 names(ret.comb) = c("GMV.LO","GMV.BOX","MARKET")
 
 # return analysis
-charts.PerformanceSummary(ret.comb,wealth.index = T,
-        lty = c(1,3,2), colorset = c("black","blue","red"),
-        cex.legend = 1.3,cex.axis = 1.3)
+# gmv_performance_chart <- charts.PerformanceSummary(ret.comb,wealth.index = T,
+#           lty = c(1,3,2), colorset = c("black","blue","red"),
+#           cex.legend = 1.3,cex.axis = 1.3)
+
+metric_list <- c( "ES"
+                 ,"maxDrawdown"
+                 ,"Return.annualized"
+                 ,"Return.cumulative"
+                 ,"SortinoRatio"
+                 ,"StdDev.annualized")
+arg.list <- list(
+   ES = list(method = "historical")
+  ,maxDrawdown = list()
+  ,Return.annualized =  list()
+  ,Return.cumulative = list()
+  ,SortinoRatio = list()
+  ,StdDev.annualized = list()
+)
+gmv_perf_table <- table.Performance(ret.comb, metrics = metric_list, interactive = F, arg.list = arg.list)$resultingtable
+gmv_perf_table <- rbind( gmv_perf_table
+                        ,SharpeRatio.annualized = SharpeRatio.annualized(ret.comb)
+                        ,SharpeRatio = SharpeRatio(ret.comb, FUN = "StdDev")
+                        ,STARR = SharpeRatio(ret.comb, FUN = "ES")
+                        )
 
 # Calculte the DIV values for the time series of the weights
 DIV.GMV.LO=DIV(wts.gmvLo)
 DIV.GMV.BOX=DIV(wts.gmvBox)
 DIV.comb=na.omit(merge(DIV.GMV.LO,DIV.GMV.BOX,all=F))
-xyplot(DIV.comb,scales=list(y="same"),main="The DIV values GMV.LO and GMV.BOX")
+div_plot <- xyplot(DIV.comb,scales=list(y="same"),main="The DIV values GMV.LO and GMV.BOX")
 ADIV.comb=sapply(DIV.comb,mean,2)
-# Print average diversification values
 
 # Calculate the TO values for the time series of weights
 TO.GMV.LO=TO(wts.gmvLo)
 TO.GMV.BOX=TO(wts.gmvBox)
 
 TO.comb=na.omit(merge(TO.GMV.LO,TO.GMV.BOX,all=F))
-xyplot(TO.comb,scales=list(y="same"),main="The TO values of GMV.LO and GMV.VOX")
+to_plot <- xyplot(TO.comb,scales=list(y="same"),main="The TO values of GMV.LO and GMV.VOX")
 ATO.comb=sapply(TO.comb,mean,2)
